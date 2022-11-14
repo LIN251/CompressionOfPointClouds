c23a77b7db084866bea6d72ce0819f44923c5311
lin251 <lz2304@vt.edu>
2022-11-14 00:23:30 -0800

    good results

--- pointCloudCompression/untitled.m
+++ pointCloudCompression/untitled.m
@@ -1,10 +1,11 @@
 % plot
-f = figure;
-axis equal;
-plot3(2, 2, 2, '.b');
+% f = figure;
+% axis equal;
+% plot3(2, 2, 2, '.b');
+% s = 5
 figure('WindowButtonDownFcn',@(src,evnt)printPos(src,evnt))
-view(-140,12);
-hold off;
+% view(-140,12);
+% hold on;
 
 
 
@@ -15,6 +16,9 @@ function printPos(src,evnt)
 %     point2d = VMtx * [clickedPt(1,:) 1]';
 %     eyepos = point2d(1:3)'
     plot3(3, 3, 3, '.b');
+    plot3(6, 6, 6, '.b');
+    plot3(8, 8, 8, '.b');
+    plot3(9, 10, 4, '.b');
     clf(src)
 
 end
